51d0371cb9e53b4f0f7ea3f73d3c5c8d2ee65a97
VITOR HENRIQUE FERNANDES BOMBA <vitor.hfbomba@senacsp.edu.br>
2018-03-02 22:06:05 -0300

    alterações 020318

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,7 @@
 // Dom7
 var $$ = Dom7;
+$$('.logoff').hide();
+$$('.login-screen-open').show();
 
 // Framework7 App main instance
 var app  = new Framework7({
@@ -56,13 +58,13 @@ var settingsView = app.views.create('#view-settings', {
 });
 
 
-// Login Screen Demo
+// Inscrever-se
 $$('#my-login-screen .Cadastrar').on('click', function () {
   var username = $$('#my-login-screen [name="email"]').val();
   var password = $$('#my-login-screen [name="senha"]').val();
 
 //Alert username and password
-  app.dialog.alert('Email: ' + username + '<br>Senha: ' + password);
+//app.dialog.alert('Email: ' + username + '<br>Senha: ' + password);
 
 firebase 
   .auth()
@@ -75,6 +77,9 @@ firebase
   .catch( function(error){
     console.error(error.code)
     console.error(error.message)
+    if (error.code =='auth/invalid-email'){
+      app.dialog.alert('E-mail inválido, por favor verifique')
+    }
     app.dialog.alert('Falha ao cadastrar, verifique o erro no console');
   //  this.$$('.toolbar-inner').innerHtml = 'Bem Vindo ' + username;
   })
@@ -83,6 +88,82 @@ firebase
   app.loginScreen.close('#my-login-screen');
 
 });
+
+// Logar
+$$('#my-login-screen .Logar').on('click', function () {
+  var username = $$('#my-login-screen [name="email"]').val();
+  var password = $$('#my-login-screen [name="senha"]').val();
+
+//Alert username and password
+//app.dialog.alert('Email: ' + username + '<br>Senha: ' + password);
+
+firebase 
+  .auth()
+  .signInWithEmailAndPassword(username,password)
+  .then( function () {
+    app.dialog.alert('Bem Vindo: ' + username);
+    this.$$('.toolbar-inner').text('Bem Vindo ' + username + 'Você está logado!');
+    $$('.logoff').show();
+    $$('.login-screen-open').hide();
+    $$('input#email').val('');
+    $$('input#senha').val('');
+  })
+  .catch( function(error){
+    console.error(error.code)
+    console.error(error.message)
+    if (error.code =='user-not-found'){
+      app.dialog.alert('Não há registro de usuário, correspondente a este odemtoficadpr. O usuário pode ter sido excluído.')
+    }
+    else if (error.code =='wrong-password'){
+      app.dialog.alert('A senha é invalida ou o usuário não possui uma senha.')
+    }
+    app.dialog.alert('Falha ao logar, verifique o erro no console');
+  })
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+});
+
+// Logoff
+$$('#my-login-screen .logoff').on('click', function () {
+  app.loginScreen.close('#my-login-screen');
+  $$('input#email').val('');
+  $$('input#senha').val('');
+
+firebase 
+  .auth()
+  .signOut()
+  .then( function () {
+    this.$$('.toolbar-inner').text('Usuario não autenticado');
+    app.dialog.alert('Usuário não autenticado');
+    app.loginScreen.close('#my-login-screen');    
+    $$('.logoff').hide();
+    $$('.login-screen-open').show();
+  })
+ 
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+});
+
+// botão fechar
+
+$$('#my-login-screen .login-screen.close').on('click', function (){
+  $$('input#email').val('');
+  $$('input#senha').val('');
+})
+$$('.logoff').on('click', function (){
+  this.$$('.toolbar-inner').text('Usuário não autenticado');
+  app.dialog.alert('Usuário não autenticado');
+  $$('input#email').val('');
+  $$('input#senha').val('');
+  $$('.logoff').hide();
+  $$('.login-screen-open').show();
+}, function(error){
+  console.error(error)
+})
+
 
   // Alert username and password
   //app.dialog.alert('Email: ' + username + '<br>Senha: ' + password);
